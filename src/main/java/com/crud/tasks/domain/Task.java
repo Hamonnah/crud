b0d782189763bb4e0d0e86fa51d094cad20b51d3
Hamonnah <hamonnah@Hamonnahs-MacBook-Air.local>
2020-09-27 21:58:39 +0200

    first commit

---
+++ src/main/java/com/crud/tasks/domain/Task.java
@@ -0,0 +1,14 @@
+package com.crud.tasks.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class Task {
+
+    private Long id;
+    private String title;
+    private String content;
+
+}
