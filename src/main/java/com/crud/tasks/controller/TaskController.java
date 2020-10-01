5f9a7b9f3a827c52d47c69851b5cc5268ec489f4
Hamonnah <hamonnah@Hamonnahs-MacBook-Air.local>
2020-10-01 14:47:26 +0200

    request mapping

--- src/main/java/com/crud/tasks/controller/TaskController.java
+++ src/main/java/com/crud/tasks/controller/TaskController.java
@@ -1,6 +1,10 @@
 package com.crud.tasks.controller;
 
 import com.crud.tasks.domain.TaskDto;
+import com.crud.tasks.mapper.TaskMapper;
+import com.crud.tasks.service.DbService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,9 +12,15 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.ArrayList;
 import java.util.List;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/v1/task")
 public class TaskController {
+
+    @Autowired
+    private DbService service;
+    @Autowired
+    private TaskMapper taskMapper;
 
     @RequestMapping(method = RequestMethod.GET,value = "getTasks")
     public List<TaskDto> getTasks() {
---
+++ src/main/java/com/crud/tasks/service/DbService.java
@@ -0,0 +1,27 @@
+package com.crud.tasks.service;
+
+import com.crud.tasks.domain.Task;
+import com.crud.tasks.repository.TaskRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+
+@Service
+public class DbService {
+
+    @Autowired
+    private TaskRepository repository;
+
+    public List<Task> getAllTasks(){
+        return repository.findAll();
+    }
+
+
+    public Task findTaskById(long id){
+        return repository.findById(id).orElseThrow(()->new RuntimeException("Not found"));
+    }
+
+
+}
