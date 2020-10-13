2bace7f03db953bb703ec020404819aceef36316
Hamonnah <hamonnah@Hamonnahs-MacBook-Air.local>
2020-10-13 11:07:51 +0200

    ex 22.3  Please enter the commit message for your changes. Lines starting

---
+++ src/main/java/com/crud/tasks/domain/Trello.java
@@ -0,0 +1,21 @@
+package com.crud.tasks.domain;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class Trello {
+
+    @JsonProperty("board")
+    int board;
+
+    @JsonProperty("card")
+    int card;
+
+}
---
+++ src/main/java/com/crud/tasks/domain/TrelloBadges.java
@@ -0,0 +1,21 @@
+package com.crud.tasks.domain;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class TrelloBadges {
+
+    @JsonProperty("attachment")
+    private TrelloAttachmentByType trelloAttachmentByType;
+
+    @JsonProperty("votes")
+    int votes;
+
+}
