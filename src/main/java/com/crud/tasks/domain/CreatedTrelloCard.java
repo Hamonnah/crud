5b6d99c9fae093ede932a5168bf9ce34bfdc80af
Hamonnah <hamonnah@Hamonnahs-MacBook-Air.local>
2020-10-13 09:21:17 +0200

    module 22.3

--- src/main/java/com/crud/tasks/domain/CreatedTrelloCard.java
+++ src/main/java/com/crud/tasks/domain/CreatedTrelloCard.java
@@ -1,2 +1,24 @@
-package com.crud.tasks.domain;public class CreatedTrelloCard {
+package com.crud.tasks.domain;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@JsonIgnoreProperties
+public class CreatedTrelloCard {
+
+    @JsonProperty("id")
+    private String id;
+
+    @JsonProperty("name")
+    private String name;
+
+    @JsonProperty("shortUrl")
+    private String shortUrl;
+
 }
--- src/main/java/com/crud/tasks/domain/TrelloListDto.java
+++ src/main/java/com/crud/tasks/domain/TrelloListDto.java
@@ -1,2 +1,24 @@
-package com.crud.tasks.domain;public class TrelloListDto {
+package com.crud.tasks.domain;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class TrelloListDto {
+
+    @JsonProperty("id")
+    private String id;
+
+    @JsonProperty("name")
+    private String name;
+
+    @JsonProperty("closed")
+    private String closed;
+
 }
